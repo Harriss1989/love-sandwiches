98c2f9edb6d305b5ccc6d8e7b0b82c5d090598e8
Harriss1989 <harriss_1@hotmail.co.uk>
2022-02-21 10:43:40 +0000

    Corrected spelling mistakes

--- run.py
+++ run.py
@@ -17,11 +17,12 @@ def get_sales_data():
     """
     Get sales figures input from the user.
     """
-    print("please enter sales data from last market.")
-    print("Data should be six numbers, seperated by commas.")
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, separated by commas.")
     print("Example: 10,20,30,40,50,60\n")
 
     data_str = input("Enter your data here: ")
     print(f"The data provided is {data_str}")
+
 
 get_sales_data()
