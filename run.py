94edc302373b6ba6574d52edcc4d9d943c57550b
Harriss1989 <harriss_1@hotmail.co.uk>
2022-02-25 13:33:33 +0000

    Add calculate stock data

--- run.py
+++ run.py
@@ -101,6 +101,23 @@ def get_last_5_entries_sales():
 
     return columns
 
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    """
+    print("Calculate stock dta...\n")
+    new_stock_data = []
+
+    for column in data :
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.appened(stock_num)
+
+    print(new_stock_data)
+
+
+
 def main():
     """
     Run all program functions
